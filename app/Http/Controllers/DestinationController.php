29f6aec305a9095b7f7ff11868021b111725c632
orsoli <fljcoli@gmail.com>
2024-10-29 17:54:52 +0100

    Implemented index function in destinationController

--- app/Http/Controllers/DestinationController.php
+++ app/Http/Controllers/DestinationController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Destination;
 use Illuminate\Http\Request;
 
 class DestinationController extends Controller
 {
     public function index(){
-        return "Hello";
+
+        $destinations = Destination::all();
+
+        return view('pages.home',compact('destinations'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,17 +1,31 @@
-{{-- Extends Layouts  --}}
+{{-- Extends Layouts --}}
 @extends('layouts.app')
 
-{{-- Links  --}}
+{{-- Links --}}
 @section('favicon','DailyTrip-logo.png')
-{{-- Page description  --}}
+{{-- Page description --}}
 @section('page-description', 'Daily Trip home page')
-{{-- Page Title  --}}
+{{-- Page Title --}}
 @section('page-name', 'Dayli Trip')
 
-{{-- Main  --}}
+{{-- Main --}}
 @section('main-content')
-    MAIN content
+<div class="row row-cols-sm-2 row-cols-md-2 row-cols-lg-3">
+  <div class="col">
+    @forelse ($destinations as $destination)
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    @empty
+    abort(404)
+    @endforelse
+  </div>
+</div>
 @endsection
-
-
-
